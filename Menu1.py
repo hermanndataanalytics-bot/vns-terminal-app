833c8582a0682aca6a8e472f9de434f6a7a42fb3
Hermann <hermanndataanalytics@gmail.com>
2026-03-11 11:04:36 +0300

    Fix indentation and save changes

--- Menu1.py
+++ Menu1.py
@@ -15,11 +15,7 @@ import streamlit.components.v1 as components
 import uuid
 import os
 import sys
-
-current_dir = os.path.dirname(os.path.abspath(__file__))
-if current_dir not in sys.path:
-    sys.path.append(current_dir)
-import si_dashboard
+import si_dashboard 
 import Forex_dashboard
 import crypto_intelligence_dashboard
 import base44_url
